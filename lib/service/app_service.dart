8b67ddb2a6c312fdd5ad2af89a0b4a01574f7918
MochAbdulAziz <43735713+MochAbdulAziz@users.noreply.github.com>
2021-07-16 07:34:23 +0700

    Add files via upload

---
+++ lib/service/app_service.dart
@@ -0,0 +1,41 @@
+
+import 'package:sekolahku/model/student_model_provider.dart';
+import 'package:sekolahku/repository/student_repository.dart';
+import 'package:sekolahku/repository/teacher_repository.dart';
+import 'package:sekolahku/service/student_service.dart';
+import 'package:sekolahku/service/teacher_service.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+StudentRepository studentRepository =
+    StudentRepository(StudentModelProvider.getInstance());
+    TeacherRepository teacherRepository =
+    TeacherRepository(StudentModelProvider.getInstance());
+StudentService studentService = StudentService(studentRepository);
+TeacherService teacherService = TeacherService(teacherRepository);
+
+class AppServices {
+   static StudentService get getStudentService {
+    return studentService;
+  }
+
+  static Future<Database> openDb(){
+    return StudentModelProvider.getInstance().openDb();
+  }
+
+  static Future<Database> closeDb(){
+    return StudentModelProvider.getInstance().closeDb();
+  }
+
+  static TeacherService get getTeacherService {
+    return teacherService;
+  }
+
+  // static Future<Database> openDbTeacher() {
+  //   print("TEST DB");
+  //   return TeacherModelProvider.getInstance().openDbTeacher();
+  // }
+
+  // static Future<void> closeDbTeacher() {
+  //   return TeacherModelProvider.getInstance().closeDbTeacher();
+  // }
+}
